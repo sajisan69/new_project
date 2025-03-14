12761de1ebc3a9568aed83570e76ddf59879e3cb
NotYour_Jisan <jisancse875@gmail.com>
2025-03-14 19:50:29 +0600

    my first project done

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String[] arr={"Jisan", "Jisan", "Jisan"};
+        for(int i=0; i< arr.length; i++){
+            System.out.println(arr[i]);
+            String str = arr[0]+arr[2];
+            System.out.println(str);
+        }
+    }
+}
