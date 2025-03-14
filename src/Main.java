ae24ced2e94c745bc611cc8076a8a2e28f5c3697
NotYour_Jisan <jisancse875@gmail.com>
2025-03-14 20:33:57 +0600

    jisan project

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) {
+        var a = "shakib";
         String[] arr={"Jisan", "Jisan", "Jisan"};
         for(int i=0; i< arr.length; i++){
             System.out.println(arr[i]);
